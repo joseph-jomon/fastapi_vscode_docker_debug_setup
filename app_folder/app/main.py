194707d867f61e5a58b04212bb1afba337e269f4
joseph-jomon <josephjomon401@gmail.com>
2024-08-30 09:02:51 +0200

    added CORS headers

--- app_folder/app/main.py
+++ app_folder/app/main.py
@@ -1,8 +1,22 @@
 from typing import Union
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
+
+
 app = FastAPI()
+
+origins = [
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware;
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],    
+)
 
 
 @app.get("/")
